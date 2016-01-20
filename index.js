cdf28c646852863c579fc312ca0f6d32bd88b68e
Bret Comnes <bcomnes@gmail.com>
2016-01-20 12:17:25 -0800

    streams n stuff

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 var path = require('path')
 var through = require('through2')
+var spy = require('through2-spy')
 var fromArray = require('from2-array')
+var map = require('through2-map')
+var sliced = require('sliced')
 var fs = require('fs')
+var os = require('os')
 
 var src = path.join(os.homedir(), '.dotfiles', 'home')
 var dest = os.homedir()
 
-fs.readDir(src, function (err, fileNames) {
-  fromArray.obj(fileNames).pipe(through.obj(function (obj, enc, cb) {
-    console.log(obj)
-    cb()
-  }))
+fs.readdir(src, function (err, fileNames) {
+  if (err) throw (err)
+  fromArray.obj(fileNames)
+    .pipe(log())
+    .pipe(genPather)
+    .pipe(log())
 })
+
+var log = spy.objCtor(function log (obj) {
+  console.log(obj)
+})
+
+function genPaths (srcDir, destDir, fileName) {
+  var src = resolveJoin(srcDir, fileName)
+  var dest = resolveJoin(destDir, '.' + fileName)
+  var relative = path.relative(path.dirname(dest), src)
+  var link = relative.indexOf('../..') === -1 ? relative : src
+
+  return {
+    name: fileName,
+    src: src,
+    dest: dest,
+    relative: relative,
+    link: link
+  }
+}
+
+//
+// resolveJoin
+// join and resolve n segments
+function resolveJoin (segment1, segment2 /* etc */) {
+  var paths = sliced(arguments)
+  return path.resolve(path.join.apply(null, paths))
+}
+
+var genPather = map.obj(genPaths.bind(null, src, dest))
